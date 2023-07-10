f36e9bb8da461a292dc9282a71d1a742eca1fa5f
maxellmilay <milaymaxell@gmail.com>
2023-07-10 23:23:38 +0800

    [UI][Maxell] added landing section

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Image from 'next/image';
+import { work, open } from '@/utils/font';
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className="flex justify-center">
+      <div className="relative w-96 h-96 mr-10">
+        <Image src="/images/milay.jpeg" alt="Profile" fill className="object-cover rounded-[50%]" />
+      </div>
+      <div className="flex flex-col items-start justify-center">
+        <h1 className={`${work.className} text-5xl mb-1`}>Maxell Milay</h1>
+        <h2 className={`${open.className} text-xl font-thin mb-8`}>Full Stack Developer</h2>
+        <button className={`${work.className} p-4 border border-solid border-white rounded`}>
+          LET&apos;S CONNECT
+        </button>
+      </div>
     </div>
   );
 }
