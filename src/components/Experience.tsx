90e53c8fe43dc9ad3e553f074e3191e3cce0f7d0
Maxell Milay <milaymaxell@gmail.com>
2024-05-23 01:42:18 +0800

    [Adjustment][Maxell] added key prop

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -15,7 +15,7 @@ function Experience() {
       <div className="relative mt-[10px] md:w-[55rem] w-[80%] after:content-[''] after:absolute after:w-[7px] after:h-full after:bg-white after:top-0 md:after:left-[50%] after:left-[30px] after:ml-[-3px] after:z-[-1] after:animate-move-line">
         {
           experiences.map((experience, index) => {
-            return <div className={`relative py-[100px] md:w-1/2 px-[50px] ${isLeft(index) ? `md:left-0` : `md:left-[50%]`} left-[30px]`}>
+            return <div key={experience.name} className={`relative py-[100px] md:w-1/2 px-[50px] ${isLeft(index) ? `md:left-0` : `md:left-[50%]`} left-[30px]`}>
               <img src={experience.logo} className={`absolute bg-white w-[60px] rounded-[50%] z-10 top-[115px] ${isLeft(index) ? 'md:right-[-30px] left-[-30px] md:left-auto' : 'left-[-30px]'}`}/>
             <div className={`relative flex flex-col flex-wrap w-[90%] ${isLeft(index) ? 'md:ml-[10%]' : "md:ml-0"} flex-wrap py-[20px] px-[30px] bg-[#121212] border rounded-[6px] text-sm`}>
               <b>{experience.name}</b>
@@ -23,8 +23,8 @@ function Experience() {
               <small>{experience.start_date} - {experience.end_date}</small>
               <div className='max-w-full'>
                 <ul className='flex flex-col list-disc pl-5'>
-                {experience.contributions.map((contrib) => {
-                  return <li className='text-xs break-words'>{contrib}</li>
+                {experience.contributions.map((contrib, index) => {
+                  return <li key={index} className='text-xs break-words'>{contrib}</li>
                 })}
                 </ul>
               </div>
